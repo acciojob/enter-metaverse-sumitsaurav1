d6583258f53ff79cb48f629fa95eff7f169a8650
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-06-18 08:12:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let statu = document.getElementById("status");
+let enterBtn = document.getElementById("enterBtn");
+
+enterBtn.addEventListener('click',(e)=>{
+  e.preventDefault();
+  statu.innerHTML="";
+  let h1 = document.createElement('h1');
+  h1.innerText= "Entered Metaverse";
+  statu.appendChild(h1);
+})
